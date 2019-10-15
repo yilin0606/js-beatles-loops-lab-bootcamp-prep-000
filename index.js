ac77b51156a57e49dc10ee866e50800b74e920ec
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-15 20:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var v=[]
+  for (var i=0;i<4;i++){
+    v.push(musicians[i]+" plays "+instruments[i])
+  }return v
+}
+
+function johnLennonFacts(array){
+ var i=0;
+ while( i<array.length){
+array[i]=array[i]+"!!!";
+i++;
+} return array
+}
+
+
+
